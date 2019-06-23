fcd1c615037ee5af660e189bdb31dcf389ad9816
Nate Goldman <ungoldman@gmail.com>
2019-06-23 13:42:02 -0700

    business edition

--- scripts/mayne.js
+++ scripts/mayne.js
@@ -1,6 +1,12 @@
+var totalGifs = 131
+var totalSounds = 20
+
+function getNumber (max) {
+  return Math.floor(Math.random() * max) + 1
+}
+
 function createGif () {
-  var max = 101
-  var n = Math.floor(Math.random() * max) + 1
+  var n = getNumber(totalGifs)
   var offset = Math.floor(Math.random() * (window.innerWidth + 100) - 100)
   var klasses = ['random', 'animate', 'slide-up', 'linear', 'd-80', 'infinite', 'gif']
 
@@ -18,7 +24,19 @@ function createGif () {
   setTimeout(function(){ img.remove() }, 20000)
 }
 
+function playSound (e) {
+  e.preventDefault()
+  var n = getNumber(totalSounds)
+  if (typeof window.Audio === 'function') {
+    var el = new Audio()
+    el.src = '/sounds/' + n + '.wav'
+    el.play()
+  }
+  createGif()
+}
+
 document.addEventListener('DOMContentLoaded', function (event) {
   $('.title').lettering()
-  setInterval(createGif, 100)
+  $('.title > span').on('click', playSound)
+  setInterval(createGif, 94)
 })
